ae7f80e34be90da22af07f2db2189dfdb9afdab3
Maria Luiza <co.maria.luiza@gmail.com>
2020-12-16 23:02:16 -0300

    initial config

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex justify-center md:justify-between items-center py-2 md:py-4">
+      <h1>Planets</h1>
+    </header>
+  )
+}
+
+export default Header
